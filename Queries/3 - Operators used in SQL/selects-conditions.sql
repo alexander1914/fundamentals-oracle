70a566aeb3d3e9a1c2a6445ce3fc8af6451f68b7
Alexander Oliveira <alexander.oliveira99@gmail.com>
2024-12-10 23:16:52 +0000

    I was cleaning code this sql queries

--- Queries/3 - Operators used in SQL/selects-conditions.sql
+++ Queries/3 - Operators used in SQL/selects-conditions.sql
@@ -2,12 +2,15 @@
 --- AND, OR, NOT are cpnditions in sql
 
 --- NOT is the word reserved 
-SELECT * FROM products P
+SELECT * 
+FROM products P
 WHERE NOT P.price > 10;
 
-SELECT * FROM products P
+SELECT * 
+FROM products P
 WHERE NOT P.product_name LIKE '%llin';
 
-SELECT * FROM products P
+SELECT * 
+FROM products P
 WHERE NOT P.price < 10
 AND P.product_id > 1;
--- Queries/3 - Operators used in SQL/selects-variables.sql
+++ Queries/3 - Operators used in SQL/selects-variables.sql
@@ -2,16 +2,20 @@
 -- & --
 -- && --
 -- UNDEFINE --
-SELECT * FROM employee E
+SELECT * 
+FROM employee E
 WHERE E.department_id = 1;
 
-SELECT * FROM employee E
+SELECT * 
+FROM employee E
 WHERE E.job_id = '&JOB';
 
-SELECT * FROM employee E
+SELECT * 
+FROM employee E
 WHERE E.hire_date >= DATE '&DATE';
 
-SELECT * FROM employee E
+SELECT * 
+FROM employee E
 WHERE E.salary = '&&SAL'
 OR E.salary = '&SAL' * 2;
 
