b1c4d49f1fcd1d8bf582a90007fe2e0140944799
Alexander Oliveira <alexander.oliveira99@gmail.com>
2024-12-11 19:54:06 +0000

    I was tring the exercise this module

--- Queries/3 - Operators used in SQL/selects-variables.sql
+++ Queries/3 - Operators used in SQL/selects-variables.sql
@@ -20,3 +20,15 @@ WHERE E.salary = '&&SAL'
 OR E.salary = '&SAL' * 2;
 
 UNDEFINE SAL;
+
+--- Exercise this module ---
+SELECT p.product_id as id, p.product_name as name, p.category,  p.discount
+FROM products p
+WHERE p.price > 50
+AND p.category = 'Electronics' 
+OR p.category = 'Appliances'
+AND (p.product_name like '%S'
+OR p.product_name like 'X%')
+AND (p.discount < 10 
+OR p.discount is null)
+ORDER BY p.product_name desc
