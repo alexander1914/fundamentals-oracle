d098f5e95bd4066ea30b28d4a5ec229dfe2afd80
Alexander Oliveira <alexander.oliveira99@gmail.com>
2025-02-12 19:04:00 +0000

    I studied more about reserved words like HAVING and pratice challenge

--- Queries/4 - Group Operations in SQL/selects.sql
+++ Queries/4 - Group Operations in SQL/selects.sql
@@ -20,7 +20,73 @@ SELECT COUNT(DISTINCT E.salary)
 FROM employee E;
 
 --- Practice Challenge: Aggregate Functions
-SELECT COUNT(E.id) AS EMPLOYEES, SUM(E.salary) AS TOTAL_SALARIES, AVG(E.salary) AS AVERAGE_SALARIES
+SELECT COUNT(E.id) AS EMPLOYEES, SUM(E.salary) AS TOTAL_SALARIES, 
+AVG(E.salary) AS AVERAGE_SALARIES, SUM(E.salary) / COUNT(E.salary) AS manual_average_salary
 FROM employee E
 WHERE E.department_id = 1;
+
 SELECT * FROM department; --- I checked the ACCOUNTING departament 
+
+SELECT E.department_id, COUNT(*) AS QUANTITY
+FROM employee E
+GROUP BY E.department_id;
+--------------------------------------------------------------------------------------------------------
+
+-- GROUP BY is a word reserved in sql to group about colunm
+
+SELECT E.department_id, COUNT(*) AS QUANTITY
+FROM employee E
+GROUP BY E.department_id;
+
+SELECT E.department_id
+FROM employee E
+GROUP BY E.department_id;
+
+SELECT DISTINCT E.department_id
+FROM employee E;
+
+SELECT MAX(SUM(E.salary)), AVG(SUM(E.salary))
+FROM employee E
+GROUP BY E.department_id;
+
+SELECT SUM(E.salary),SUM(E.salary)
+FROM employee E
+GROUP BY E.department_id;
+
+
+--- Practice Challenge: Grouping Rows
+SELECT bonus,
+   COUNT(*) AS employees,
+   MAX(salary) AS greatest_salary
+FROM employee
+WHERE birthdate < DATE '1995-01-01'
+GROUP BY bonus
+ORDER BY bonus;
+
+--- HAVING is a word reserved in sql to verify like if
+SELECT E.department_id, COUNT(*) 
+FROM employee E
+GROUP BY E.department_id
+HAVING COUNT(*) > 1;
+
+SELECT E.department_id, COUNT(*) 
+FROM employee E
+GROUP BY E.department_id
+HAVING E.department_id = 3;
+
+SELECT E.department_id, MIN(E.salary) 
+FROM employee E
+WHERE E.department_id IN (3,4)
+GROUP BY E.department_id
+HAVING MIN(E.salary) > 2000;
+
+--- Practice Challenge: Grouping Rows
+SELECT department_id, MIN(E.salary), MAX(E.salary), AVG(E.bonus)
+FROM employee E
+WHERE bonus IS NOT NULL
+GROUP BY department_id
+HAVING MIN(E.salary) < 2000
+   OR MAX(E.salary) > 4000
+ORDER BY MIN(salary) DESC;
+
+
