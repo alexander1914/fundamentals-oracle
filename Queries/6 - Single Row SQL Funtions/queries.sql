88fa093390095147494db3eec43fc60ef7ae4817
Alexander Oliveira <alexander.oliveira99@gmail.com>
2025-09-01 21:07:13 +0100

    I was learning more about functions on sql oracle today - last parte

--- Queries/6 - Single Row SQL Funtions/queries.sql
+++ Queries/6 - Single Row SQL Funtions/queries.sql
@@ -112,3 +112,189 @@ FROM dual;
 SELECT D.*, TO_DATE(D.last_employee_id,'mm')
 FROM DEPARTMENT D
 WHERE VALIDATE_CONVERSION(TO_CHAR(D.last_employee_id) AS DATE, 'mm') = l;
+
+SELECT * 
+FROM employee e
+WHERE e.hire_date = TO_DATE('01-10-2014', 'DD-MM-YYYY');
+
+SELECT * 
+FROM employee e
+WHERE e.hire_date = TO_DATE('01-10-2014', 'DD-MM-YYYY', 
+'NLS_DATE_LANGUAGE=''portuguese''');
+
+--- DECODE ---
+SELECT name, 'DEPT: ' || DECODE(e.department_id,3,'IT',4,'HR','OTHER')
+FROM employee e;
+
+SELECT d.name, d.monthly_budget/DECODE(d.id,1,4,5) AS donation
+FROM department d;
+
+SELECT e.salary, DECODE(SIGN(e.salary-3000),1,'GREATER THAN 3000', 'NOT GREATER')
+FROM employee e;
+
+SELECT e.name,
+    CASE e.department_id
+        WHEN 3
+            THEN 'IT'
+        WHEN 4
+            THEN 'HR'
+        ELSE
+            'OTHER'
+    END as dept_id        
+FROM employee e;
+
+SELECT e.name,
+    CASE 
+        WHEN e.department_id = 1 AND e.hire_date >= DATE '2015-01-01'      
+             THEN 'NEW ACCOUNTANT'
+        WHEN e.job_id = 'IT_PROG' AND e.salary < 3000 
+            THEN 'LOW PAY PROGRAMMMER'
+        ELSE
+            'OTHER'
+    END as type        
+FROM employee e;
+
+SELECT SYSDATE
+    ,SYSDATE-365
+    ,ADD_MONTHS(SYSDATE,1)-SYSDATE
+    ,TO_CHAR(SYSDATE+6/24, 'hh24:mi:ss')
+FROM DUAL;
+
+SELECT e.name
+    ,TO_CHAR(e.birthdate+5/(24*60*60), 'dd-mm-yyyy hh24:mi:ss')
+FROM employee e;
+
+SELECT e.name
+    ,TO_CHAR(e.birthdate+5.787037037037037e-5, 'dd-mm-yyyy hh24:mi:ss')
+FROM employee e;
+
+--- TEST ---
+Select Id,
+  initcap(name) AS name,
+  Birthdate,
+  Replace(Phone,'.','-') As Phone,
+  Salary,
+  Department_Id,
+  Hire_Date,
+  Job_Id,
+  Lower(Email) || '@gmail.com' As Email,
+  bonus,
+  Translate(Substr(name,1,instr(name,' ')-1),'XAEIOU','X') As code
+FROM employee;
+
+SELECT  Id,
+  name,
+  Birthdate,
+  Phone,
+  SUBSTR(phone,INSTR(phone,'.')+1,INSTR(phone,'.',1,2)-INSTR(phone,'.')-1) As Phone_segment,
+  Salary,
+  Department_Id,
+  Hire_Date,
+  Job_Id,
+  Email,
+  bonus
+From Employee
+WHERE hire_date >= date '2010-01-01'
+ORDER BY salary DESC;
+
+WITH emp_data AS
+  (
+    SELECT Name, Job_Id,Salary, salary / 30 AS daily_salary
+    FROM Employee
+    WHERE Hire_Date > DATE '2010-12-31'
+      OR Department_Id = 3
+  )
+Select e.*,
+Round(Daily_Salary),
+Trunc(Daily_Salary),
+Ceil(Daily_Salary),
+Floor(Daily_Salary)
+FROM Emp_Data e
+WHERE Daily_Salary = TRUNC(Daily_Salary);
+
+Select Name, Job_Id,Salary, 
+  Salary / 30 As Daily_Salary,
+  Round(Salary / 30),
+  Trunc(Salary / 30),
+  Ceil(Salary / 30),
+  Floor(Salary / 30)
+FROM Employee
+WHERE
+  (
+    Hire_Date > DATE '2010-12-31'
+    OR Department_Id = 3
+  )
+  AND salary / 30 = TRUNC(salary / 30);
+
+SELECT id,name,hire_date,
+  TRUNC(MONTHS_BETWEEN(SYSDATE,hire_date)) AS months_worked,
+  ADD_MONTHS(hire_date,6) AS raise_earned_date,
+  NEXT_DAY(hire_date,'monday') AS induction_date,
+  TRUNC(ADD_MONTHS(hire_date,1),'month') AS newsletter_date
+FROM employee
+WHERE department_id != 3
+
+SELECT name,birthdate,phone,
+  TO_CHAR(department_id,'fm0000') AS department_id,
+  TO_CHAR(salary,'fmL99,990.00') AS salary,
+  TO_DATE(TO_CHAR(birthdate,'DDMMYYYY') DEFAULT NULL ON CONVERSION ERROR,'MMDDYYYY') AS alt_birthdate
+FROM employee
+WHERE birthdate > DATE '1970-12-31'
+  AND phone IS NOT NULL;
+
+  SELECT name,
+  TO_CHAR(birthdate,'DD/MM') AS birthdate,
+  TO_CHAR(hire_date,'fmMONTH YYYY') AS hire_date,
+  TO_CHAR(TO_NUMBER(SUBSTR(phone,-4)),'L9990.00') AS bonus,
+  TO_CHAR(TO_DATE(SUBSTR(phone,-1) DEFAULT '12' ON CONVERSION ERROR,'mm'),'month') AS bonus_month
+FROM employee
+WHERE
+  (
+    hire_date < DATE '2015-01-01'
+    AND salary > 2500
+  )
+  OR
+  (
+    TO_CHAR(hire_date,'YYYY') = '2015'
+    AND salary < 3000
+  );
+
+  SELECT id,name,salary AS current_salary,
+  salary + (salary * 
+    DECODE(department_id,1,0.10,2,0.15,0.20)
+  ) AS new_salary_1,
+  salary + (salary *
+    CASE department_id
+      WHEN 1  -- Accounting
+        THEN 0.10
+      WHEN 2  -- Marketing
+        THEN 0.15
+      ELSE 0.20
+    END
+  ) AS new_salary_2
+FROM employee;
+
+SELECT id,name,salary,
+  TO_CHAR(hire_date,'YYYY') AS hire_year,
+  CASE
+    WHEN salary < 2500
+      THEN 'A'
+    WHEN salary >= 2500 AND salary < 4000
+      THEN 'B'
+    WHEN salary >= 4000 AND hire_date < DATE '2014-01-01'
+      THEN 'C'
+    WHEN salary >= 4000 AND TO_CHAR(hire_date,'YYYY') IN ('2014','2015')
+      THEN 'D'
+  END AS classification
+FROM employee
+WHERE department_id != 2
+  AND phone IS NOT NULL
+ORDER BY salary,hire_date;
+
+SELECT id,name,
+  birthdate + (2 * 365) AS second_birthday,
+  (hire_date - birthdate) * 24 AS hours_old_when_hired
+FROM employee
+WHERE hire_date >= DATE '1980-01-01'
+  AND phone LIKE '1%'
+ORDER BY department_id,salary DESC;
